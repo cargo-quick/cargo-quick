11ec1c45eb2bc3a45d906aca4da37879215b71fd
David Laban <david.laban@red-badger.com>
2021-03-12 17:02:26 +0000

    noop transform by walking the tree

--- cargo-quickbuild/src/main.rs
+++ cargo-quickbuild/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::BTreeSet;
+
+use cargo_lock::{
+    dependency::graph::{Graph, NodeIndex},
+    Error, Lockfile, Package,
+};
+
+fn main() -> Result<(), Error> {
+    let lockfile = Lockfile::load("Cargo.lock").unwrap();
+    let tree = lockfile.dependency_tree()?;
+    let graph = tree.graph();
+
+    for index in tree.roots().iter() {
+        let subtree = make_subtree(&lockfile, &graph, *index).unwrap();
+        println!("{}", subtree.to_string());
+        break;
+    }
+    Ok(())
+}
+
+fn make_subtree(lockfile: &Lockfile, graph: &Graph, index: NodeIndex) -> Result<Lockfile, Error> {
+    let packages = walk_subtree(graph, index);
+    let mut set = BTreeSet::new();
+    set.extend(packages.into_iter());
+    let packages = set.into_iter().collect();
+
+    Ok(Lockfile {
+        packages,
+        ..lockfile.clone()
+    })
+}
+
+fn walk_subtree(graph: &Graph, index: NodeIndex) -> Vec<Package> {
+    let mut result = vec![graph[index].clone()];
+    for ix in graph.neighbors(index) {
+        result.extend(walk_subtree(graph, ix).into_iter());
+    }
+    result
 }
