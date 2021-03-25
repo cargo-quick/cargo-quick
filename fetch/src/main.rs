ca0c23050a2696d45d99bade63c24814ff017b95
Matt Paul <mattpaul001@icloud.com>
2021-03-25 15:51:09 +0000

    Fetch multiple cargo.lock files

--- fetch/src/main.rs
+++ fetch/src/main.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
-use std::fs::File;
+use std::fs::{create_dir_all, rename, File};
+use std::io::copy;
+use tempfile::Builder;
 
 use serde::Deserialize;
 
@@ -18,7 +20,45 @@ fn get_first_arg() -> Result<std::ffi::OsString, Box<dyn std::error::Error>> {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+async fn fetch_and_write_file(
+    repo_root: &str,
+    repo: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let branch = "master";
+    let target = format!(
+        "https://raw.githubusercontent.com/{}/{}/Cargo.lock",
+        repo, branch
+    );
+
+    let response = reqwest::get(target).await?;
+
+    let tmp_dir = Builder::new().tempdir()?;
+    let fname = response
+        .url()
+        .path_segments()
+        .and_then(|segments| segments.last())
+        .and_then(|name| if name.is_empty() { None } else { Some(name) })
+        .unwrap_or("tmp.bin");
+
+    let fname = tmp_dir.path().join(fname);
+    let mut temp_destination = { File::create(&fname)? };
+
+    let content = response.text().await?;
+    copy(&mut content.as_bytes(), &mut temp_destination)?;
+
+    let perm_directory = format!("{}/data/{}", repo_root, repo);
+    create_dir_all(&perm_directory)?;
+
+    let perm_dest_path = format!("{}/Cargo.lock", &perm_directory);
+    rename(fname, &perm_dest_path)?;
+
+    println!("Cargo.lock retrieved: {}", &perm_dest_path);
+
+    Ok(())
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error>> {
     let repo_root = get_first_arg()?;
     let file_path = std::path::Path::new(&repo_root).join("data/github.csv".to_string());
     let file = File::open(file_path)?;
@@ -34,13 +74,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let repo_root_str = repo_root.to_str().unwrap();
 
     for record in valid_records {
-        let path = format!("{}/data/{}/Cargo.toml", repo_root_str, record.name);
-
-        println!("The path is {}", path);
-
+        let path = format!("{}/data/{}/Cargo.lock", &repo_root_str, &record.name);
         if std::path::Path::new(&path).exists() {
             continue;
         }
+
+        fetch_and_write_file(repo_root_str, &record.name)
+            .await
+            .unwrap();
     }
 
     Ok(())
