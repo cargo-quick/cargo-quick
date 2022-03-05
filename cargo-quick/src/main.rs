41c88c493fb066ae448049e5b9f36d14fd6e1d56
David Laban <alsuren@gmail.com>
2022-03-05 12:54:01 +0000

    cargo quick install -> cargo-quickinstall

--- cargo-quick/src/main.rs
+++ cargo-quick/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<_> = std::env::args().collect();
+    if args[0] == "quick" {
+        args.remove(1);
+    }
+    match args[1].as_str() {
+        "install" => {
+            let exitcode = std::process::Command::new("cargo-quickinstall")
+                .args(&args[2..])
+                .status()
+                .unwrap();
+            std::process::exit(exitcode.code().unwrap_or(1));
+        }
+        other => todo!("TODO: implement {other}"),
+    }
 }
