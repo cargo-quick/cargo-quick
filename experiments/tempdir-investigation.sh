5e7fba2c7db70227e81b37566a5eb769b0feb3f0
David Laban <alsuren@gmail.com>
2022-03-05 22:32:40 +0000

    delete commented-out code

--- experiments/tempdir-investigation.sh
+++ experiments/tempdir-investigation.sh
@@ -10,7 +10,7 @@ commit() {
 
     gls --full-time -Rl target > timestamps.txt
     git add .
-    git commit --allow-empty -am "$LAST_COMMIT_MESSAGE"
+    git commit --allow-empty -am "TMP: $LAST_COMMIT_MESSAGE"
 }
 
 REPO_ROOT="$PWD"
@@ -20,24 +20,7 @@ rm -rf /tmp/cargo-quickbuild-hack
 
 mkdir /tmp/cargo-quickbuild-hack
 
-# TODO: think about how the features get resolved.
 cargo init /tmp/cargo-quickbuild-hack/hack
-### This doesn't work:
-# # The [dependencies] section is at the bottom of the Cargo.toml
-# cargo tree -p regex-automata --prefix none \
-#     | sed -e 's/ v/ = "=/' -e 's/$/"/' \
-#     >> /tmp/cargo-quickbuild-hack/hack/Cargo.toml
-# cat experiments/breakdown/Cargo.toml \
-#     | grep -B1000000 -F [dependencies] \
-#     > /tmp/cargo-quickbuild-hack/hack/Cargo.toml
-
-# For some reason, if I exclude csv then the build doesn't find regex-automata at all
-# for package in `cargo tree -p regex-automata --invert --prefix=none | sed 's/ .*//'`; do
-#     cat experiments/breakdown/Cargo.toml \
-#         | (grep "^$package" || true) \
-#         | (grep -v "^globwalk" || true) \
-#         >> /tmp/cargo-quickbuild-hack/hack/Cargo.toml
-# done
 
 # TODO: work out how to infer the set of features programmatically.
 # It looks like `cargo metadata` will give us what we need here.
@@ -60,18 +43,6 @@ cp Cargo.lock /tmp/cargo-quickbuild-hack/
     cargo build -p regex-automata
     tar --format=pax -c target > /tmp/target.tar
 )
-
-# TODO: investigate whether you can delete some of the workspace crates,
-# sand still get the same build result.
-# for file in experiments/breakdown/Cargo.toml experiments/breakdown/src/main.rs # Cargo.lock
-# do
-#     rm -rf /tmp/cargo-quickbuild-hack/$file
-#     mkdir -p /tmp/cargo-quickbuild-hack/$file
-#     rm -rf /tmp/cargo-quickbuild-hack/$file
-#     cp $file /tmp/cargo-quickbuild-hack/$file
-# done
-
-
 
 rm -rf target
 tar -x -f /tmp/target.tar
