6c6a67974625a33d0ca74c9f69b91bae358574b3
David Laban <alsuren@gmail.com>
2022-03-05 21:19:59 +0000

    seems like you need the Cargo.toml *and* the Cargo.lock?

--- experiments/tempdir-investigation.sh
+++ experiments/tempdir-investigation.sh
@@ -19,21 +19,39 @@ rm -rf target
 rm -rf /tmp/cargo-quickbuild-hack
 
 mkdir /tmp/cargo-quickbuild-hack
-cargo init /tmp/cargo-quickbuild-hack
+
+# TODO: think about how the features get resolved.
+cargo init /tmp/cargo-quickbuild-hack/hack
+### This doesn't work:
+# # The [dependencies] section is at the bottom of the Cargo.toml
+# cargo tree -p regex-automata --prefix none \
+#     | sed -e 's/ v/ = "=/' -e 's/$/"/' \
+#     >> /tmp/cargo-quickbuild-hack/hack/Cargo.toml
+cp experiments/breakdown/Cargo.toml /tmp/cargo-quickbuild-hack/hack/
+
+# For some reason, workspace vs non-workspace seems to matter?
+cat > /tmp/cargo-quickbuild-hack/Cargo.toml <<EOF
+[workspace]
+members = ["hack"]
+EOF
+cp Cargo.lock /tmp/cargo-quickbuild-hack/
+
+(
+    cd /tmp/cargo-quickbuild-hack
+
+    cargo build -p regex-automata
+    tar --format=pax -c target > /tmp/target.tar
+)
+
 # TODO: investigate whether you can delete some of the workspace crates,
 # sand still get the same build result.
-for file in `git ls-files | grep -E '(Cargo|lib.rs|main.rs)' `; do
+for file in Cargo.lock experiments/breakdown/Cargo.toml experiments/breakdown/src/main.rs; do
     rm -rf /tmp/cargo-quickbuild-hack/$file
     mkdir -p /tmp/cargo-quickbuild-hack/$file
     rm -rf /tmp/cargo-quickbuild-hack/$file
     cp $file /tmp/cargo-quickbuild-hack/$file
 done
 
-(
-    cd /tmp/cargo-quickbuild-hack
-    cargo build -p regex-automata
-    tar --format=pax -c target > /tmp/target.tar
-)
 
 
 rm -rf target
