50f8653ee38826120310c303b111f299b8aa9b57
David Laban <alsuren@gmail.com>
2022-08-06 18:35:49 +0100

    hunt-problematic-files.sh git-repo filename
    At some point I will make a thing that searches the whole repo, but not today

--- experiments/hunt-problematic-files.sh
+++ experiments/hunt-problematic-files.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-OTHER_GIT_REPO=${1?USAGE $0 path/to/rust/crate/repo}
+OTHER_GIT_REPO=${1?USAGE $0 path/to/rust/crate/repo target/problematic/file}
+PROBLEM=${2?USAGE $0 path/to/rust/crate/repo target/problematic/file}
+
 TARBALL_DIR="$HOME/tmp/$(basename "$OTHER_GIT_REPO")"
 
 # nightly gives backtraces for anyhow errors
@@ -13,9 +15,9 @@ rm -rf "$OTHER_GIT_REPO/target"
 \in "$OTHER_GIT_REPO" cargo quickbuild "$TARBALL_DIR" && exit 0
 
 for f in "$TARBALL_DIR"/*.tar ; do
-    listing=$(~/.nix-profile/bin/tar --list -vv --full-time -f $f target/.rustc_info.json 2>/dev/null) \
+    listing=$(~/.nix-profile/bin/tar --list -vv --full-time -f $f "$PROBLEM" 2>/dev/null) \
       || continue
     echo "$f"
     echo "$listing"
-    ~/.nix-profile/bin/tar -x --to-stdout -f $f target/.rustc_info.json
+    ~/.nix-profile/bin/tar -x --to-stdout -f $f "$PROBLEM"
 done
