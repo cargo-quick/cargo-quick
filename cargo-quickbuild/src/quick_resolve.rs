2c07c3036f1559eec4fccb30d548e619b23eef25
David Laban <alsuren@gmail.com>
2022-08-09 16:21:50 +0100

    create_quick_resolve() helper

--- cargo-quickbuild/src/quick_resolve.rs
+++ cargo-quickbuild/src/quick_resolve.rs
@@ -1,12 +1,19 @@
 use std::collections::BTreeSet;
+use std::collections::HashMap;
+
+use anyhow::Result;
+use cargo::core::compiler::RustcTargetData;
 
 use cargo::core::dependency::DepKind;
+use cargo::core::Package;
 use cargo::core::{PackageId, Workspace};
 use cargo::ops::WorkspaceResolve;
+use cargo::ops::{CompileOptions, Packages};
+
 use itertools::Itertools;
 
 use crate::vendor::tree::graph::Graph;
-use crate::vendor::tree::EdgeKind;
+use crate::vendor::tree::{Charset, EdgeKind, Prefix, Target, TreeOptions};
 
 /// A wrapper around the cargo core resolve machinery, to make cargo-quickbuild work.
 /// Probably won't be all that quick ;-)
@@ -66,6 +73,69 @@ impl<'cfg, 'a> QuickResolve<'cfg, 'a> {
     }
 }
 
+pub fn create_quick_resolve<'cfg, 'a>(
+    ws: &'a Workspace<'cfg>,
+    options: &CompileOptions,
+    workspace_resolve: &'a cargo::ops::WorkspaceResolve<'cfg>,
+) -> Result<QuickResolve<'cfg, 'a>, anyhow::Error> {
+    let requested_kinds = &options.build_config.requested_kinds;
+    let target_data = RustcTargetData::new(ws, requested_kinds)?;
+    let package_map: HashMap<PackageId, &Package> = workspace_resolve
+        .pkg_set
+        .packages()
+        .map(|pkg| (pkg.package_id(), pkg))
+        .collect();
+    let packages = clone_packages(&options.spec);
+    let opts = TreeOptions {
+        cli_features: options.cli_features.clone(),
+        packages,
+        target: Target::Host,
+        edge_kinds: [
+            EdgeKind::Dep(DepKind::Normal),
+            EdgeKind::Dep(DepKind::Build),
+        ]
+        .into_iter()
+        .collect(),
+        invert: Default::default(),
+        pkgs_to_prune: Default::default(),
+        prefix: Prefix::None,
+        no_dedupe: Default::default(),
+        duplicates: Default::default(),
+        charset: Charset::Ascii,
+        format: Default::default(),
+        graph_features: Default::default(),
+        max_display_depth: Default::default(),
+        no_proc_macro: Default::default(),
+    };
+    let graph = crate::vendor::tree::graph::build(
+        ws,
+        &workspace_resolve.targeted_resolve,
+        &workspace_resolve.resolved_features,
+        &options.spec.to_package_id_specs(ws)?,
+        &options.cli_features,
+        &target_data,
+        requested_kinds,
+        package_map,
+        &opts,
+    )
+    .unwrap();
+    let resolve = QuickResolve {
+        ws,
+        workspace_resolve,
+        graph,
+    };
+    Ok(resolve)
+}
+
+fn clone_packages(packages: &Packages) -> Packages {
+    match packages {
+        Packages::Default => Packages::Default,
+        Packages::All => Packages::All,
+        Packages::OptOut(vec) => Packages::OptOut(vec.clone()),
+        Packages::Packages(vec) => Packages::Packages(vec.clone()),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
@@ -99,7 +169,7 @@ mod tests {
 
         let opts = TreeOptions {
             cli_features: options.cli_features.clone(),
-            packages: options.spec.clone(),
+            packages: clone_packages(&options.spec),
             target: Target::Host,
             edge_kinds: [
                 EdgeKind::Dep(DepKind::Normal),
